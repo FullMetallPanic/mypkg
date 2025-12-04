fac1c3ae67df334b29d5d78401c2ad7ff331b8f4
Full Metal Panic! <x78618741@gmail.com>
2025-12-04 11:27:55 +0900

    変更済み

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -3,17 +3,19 @@ import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
-
 def generate_launch_description():
 
-     talker = launch_ros.actions.Node(
-         package='mypkg',      #パッケージの名前を指定
-         executable='talker',  #実行するファイルの指定
-         )
-     listener = launch_ros.actions.Node(
-         package='mypkg',
-         executable='listener',
-         output='screen'        #ログを端末に出すための設定
-         )
+    talker = launch_ros.actions.Node(
+        package='mypkg',
+        executable='talker',)
 
-     return launch.LaunchDescription([talker, listener])
+    listener = launch_ros.actions.Node(
+        package='mypkg',
+        executable='listener',
+        output='screen',
+    )
+
+    return launch.LaunchDescription([
+        talker,
+        listener
+    ])
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Query
+from person_msgs.srv import Query
 
 rclpy.init()
 node = Node("talker")
@@ -8,14 +8,14 @@ node = Node("talker")
 
 def cb(request, response):
     if request.name == "春川翔":
-          responce.age = 20
+          response.age = 20
     else:
-         responce.age = 255
+         response.age = 255
 
     return response
 
 
 
 def main():
-    node.create_timer(0.5, cb)
+    srv = node.create_service(Query, "query", cb)
     rclpy.spin(node)
