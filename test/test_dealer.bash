011289dc06ab84e790fd6c873c1751bb527eb071
Full Metal Panic! <x78618741@gmail.com>
2025-12-30 22:30:33 +0900

    早く

--- test/test_dealer.bash
+++ test/test_dealer.bash
@@ -1,23 +1,24 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2025 Hayato Matsumoto
 # SPDX-License-Identifier: BSD-3-Clause
-
 set -e
 
-WS_DIR=${1:-$HOME/ros2_ws}
-
-cd "$WS_DIR"
-colcon build --event-handlers console_direct+
-
+source /opt/ros/humble/setup.bash
 source install/setup.bash
 
-timeout 10 ros2 run mypkg poker_dealer &
+
+ros2 run mypkg dealer &
 DEALER_PID=$!
 
-sleep 2
 
-timeout 10 bash -c "source install/setup.bash && ros2 topic echo --once /poker_table"
+sleep 3
 
-if ps -p $DEALER_PID > /dev/null 2>&1; then
-    kill $DEALER_PID
+
+if ! ros2 topic echo --once /poker_table; then
+    echo "WARNING: /poker_table not published"
+    kill $DEALER_PID || true
+    exit 1
 fi
+
+
+kill $DEALER_PID
